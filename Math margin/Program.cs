20735f4af6015e834de98c737fdc37ab801f24db
NecrosUA <rostislav.email@gmail.com>
2020-12-06 14:26:16 +0200

    git commit EDU

---
+++ Math margin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Math_margin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
